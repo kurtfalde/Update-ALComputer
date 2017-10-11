f1b24365b20363ba20e24080c52354822bac192f
kurtfalde <engineerfalde@yahoo.com>
2017-10-11 12:00:46 -0400

    Update Update-ALComputer.ps1

--- Update-ALComputer.ps1
+++ Update-ALComputer.ps1
@@ -2,9 +2,7 @@
 
 .SYNOPSIS
 Retrieves computer names from a specified group in Active Directory and remotely performs various operations against these
-systems in support of AppLocker configuration.
-Requires administrative access to the remote systems
-Requires Active Directory cmdlets 
+systems in support of AppLocker configuration. Requires administrative access to the remote systems Requires Active Directory cmdlets 
 
 .PARAMETER Computer
 Specifies an input name of a target workstation
@@ -14,19 +12,20 @@ Specifies the name of the computer group that should be used to query and perfor
 
 .PARAMETER klist
 If set this will perform a purge of all system kerberos tickets on the target systems forcing them to get new tickets 
-and subsequently new computer group membership information
+ and subsequently new computer group membership information
 
-.PARAMETER gpupdate
+u.PARAMETER gpupdate
 If set this will perform a forced group policy refresh of the computer settings on the target systems
 
 .PARAMETER startal
 If set this will set the appidsvc service to automatic and attempt to start the appidsvc on the target systems
 
+
 .EXAMPLE
 
-Update-ALcomputer -ADGroupName AppLocker-Audit -klist -gpupdate -startal
+ Update-ALcomputer -ADGroupName AppLocker-Audit -klist -gpupdate -startal
 
-#>
+ #>
 
 function Update-ALComputer {
     [CmdletBinding()]
